3a0b9b630ef0ca8e4ca18be763cb079a4faae9cf
Heinrich Mises <henrique.lapf@gmail.com>
2024-03-30 10:49:16 -0300

    add content change in same page

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,8 @@
 
 <template>
     <div class="px-4 flex justify-between fixed bottom-0 bg-[#248588] w-full h-12 text-white" >
-        <button type="button" @click="">Movies</button>
-        <button type="button" @click="">Tv Shows</button>
+        <button type="button" @click="$emit('changepage', 'movies' )">Movies</button>
+        <button type="button" @click="$emit('changepage', 'tvshows' )">Tv Shows</button>
         <button type="button" ><a href="/signin">Profile</a></button>
     </div>
 </template>
